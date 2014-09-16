f69f9c29138743fc5e6b9f8b73c49eb6c53b2960
Margit <margit.pilden@gmail.com>
2014-09-16 12:51:23 +0300

    Booleans

--- index.php
+++ index.php
@@ -8,7 +8,16 @@
 </head>
 
 <body>
-
+	<h2>Booleans</h2>
+	<?php $result1 = True?>
+	<?php $result2 = False?>
+	<?php echo $result1?><br>
+	<?php echo $result2?>
+	<?php echo "Result 1: {$result1}"?><br>
+	<?php echo "Result 2: {$result2}"?><br>
+	<?php echo "Kas result 1 on boolean? " .  is_bool($result1)?><br>
+	<?php echo "Kas result 2 on boolean? " .  is_bool($result2)?><br>
+	
 </body>
 
 </html>
